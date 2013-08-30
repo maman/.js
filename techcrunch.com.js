3cd30712f66fc037c45bdf01d00f9bda0a498d51
Achmad Mahardi <achmad@mahardi.me>
2013-08-30 17:35:07 +0700

    add couple more sites

---
+++ techcrunch.com.js
@@ -0,0 +1,6 @@
+/*
+ TechCrunch Cleanup
+*/
+
+css = "#module-header,.top-featured-posts,.widget_tc_banner_nav_widget,.right-container,.lazy-share-widget,.module-footer,.publication-info,.module-sponsored-ads,.more-link{display:none!important}#page-container{padding-top:50px}.column-container,#paging-below{width:668px}h1,h2,h3,h4,h5,h6,.headline,.load-more-results,.menu-item,.page-nav{font-family:'Helvetica Neue',Helvetica,sans-serif;letter-spacing:inherit}.module-post .headline,.module-post .headline a,.module-post-detail h1.headline{font-weight:300!important;font-size:35px;letter-spacing:0;line-height:40px}.module-post .headline{margin-top:0;margin-bottom:30px}.module-post{padding:50px 0}.module-post .body-copy,.module-post .entry-summary{color:#333;font-size:16px;line-height:22px}";
+GM_addStyle(css);
