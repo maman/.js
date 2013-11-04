678535df3aa21b9e4ce527e1f70ba6b1304aa962
Achmad Mahardi <achmad@mahardi.me>
2013-11-05 00:09:41 +0700

    add arstechnica, and remove w3school from google search result

---
+++ arstechnica.com.js
@@ -0,0 +1,6 @@
+/*
+ * Make Arstechnica more accesible
+ */
+
+css = "article{float: none !important; width: 100% !important; padding-right: 0 !important; border-right: none !important} ul#recommendations>li{width: 225px; margin: 0 26px 50px 0} ul#recommendations > li a { display: block; } ul#recommendations > li img {width: 100% !important; height: auto !important;} figure.image.center.full-width {width: 100% !important} figure.image img {width: 100% !important; height: auto !important} #article-sidebar {display: none;}"
+GM_addStyle(css);
---
+++ github.com.js
@@ -0,0 +1,1 @@
+$('.repository-lang-stats-graph').remove();
